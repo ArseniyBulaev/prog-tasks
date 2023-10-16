f74ff5bd60f1507acf69e49548216e16464cef1a
Arseny Bulaev <seni190999@mail.ru>
2023-10-16 20:42:19 +0300

    recursiveCount

--- simpleRecursionGA4/recursion.cpp
+++ simpleRecursionGA4/recursion.cpp
@@ -18,8 +18,24 @@ void recursiveSumTest(){
     cout << (recursiveSum({1}) == 1) << endl;
 }
 
+int recursiveCount(vector<int> array){
+    if (array.empty()) return 0;
+    array.pop_back();
+    return 1 + recursiveCount(array);
+}
+
+void recursiveCountTest(){
+    vector<vector<int>> tests = {{}, {1}, {1, 2, 3}, {10,10,10,10}};
+    for (const vector<int> & test : tests)
+    {
+       cout << (recursiveCount(test) == test.size()) << endl;
+    }
+}
+
 int main(){
 
     
+    
+
     return 0;
 }
