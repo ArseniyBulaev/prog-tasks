5d48e8c6608e515d8ec9869bb3f12d60727bd31d
Arseny Bulaev <seni190999@mail.ru>
2023-11-01 08:43:20 +0300

    move nearest neighbor heuristic to separate file

--- algorithm design manual/chapter 1/26 travel salesman problem/main.cpp
+++ algorithm design manual/chapter 1/26 travel salesman problem/main.cpp
@@ -5,54 +5,10 @@
 #include <algorithm>
 
 #include "Point.h"
+#include "nearest_neighbor.h"
 
 using std::vector, std::set;
 using std::cout, std::endl;
-
-
-// Эвристика ближайшей точки
-vector<Point> nearest_neighbor(set<Point> available_points){
-    // Маршрут
-    vector<Point> route;
-    // Генератор случайных чисел
-    std::random_device dev;
-    // Алгоритм для генерации
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(0, available_points.size() - 1);
-    // Из множества P выбираем и посещаем произвольную точку p_0
-    auto it = std::begin(available_points);
-    std::advance(it, dist(rng));
-    Point p = *it;
-    // Удаляем выбранную точку из доступных (Посещаем её)
-    available_points.extract(p);
-    route.push_back(p);
-    
-    // Пока остаются непосещённые точки
-    while(!available_points.empty()){
-        const Point * nearest = nullptr;
-        double distance_to_nearest;
-        // Выбираем точку ближаюшую к p
-        for (const Point & some_point : available_points){
-            // Расстояние до некоторой точки
-            double distance = Point::calculate_distance(p, some_point);
-            if (nearest){
-                if (distance < distance_to_nearest){
-                    nearest = &some_point;
-                    distance_to_nearest = distance;
-                }
-            }
-            else{
-                nearest = &some_point;
-                distance_to_nearest = distance;
-            }
-        } 
-        // Посещаем ближайшую точку
-        p = *nearest;
-        available_points.extract(p);
-        route.push_back(p);
-    }
-    return route;
-}
 
 
 
---
+++ algorithm design manual/chapter 1/26 travel salesman problem/nearest_neighbor.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <set>
+#include <random>
+
+#include "Point.h"
+
+std::vector<Point> nearest_neighbor(std::set<Point> available_points);
