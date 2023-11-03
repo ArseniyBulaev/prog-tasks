8ed4cbbcd74aff6104c7ff1ea4e7d7d79f3824c7
Arseny Bulaev <seni190999@mail.ru>
2023-11-03 19:34:39 +0300

    generate all subsets of k length

--- algorithm design manual/chapter 1/27. lotto problem/main.cpp
+++ algorithm design manual/chapter 1/27. lotto problem/main.cpp
@@ -1,6 +1,9 @@
 #include <set>
+#include <vector>
+#include <iostream>
+#include <cmath>
 
-using std::set;
+using std::set, std::vector;
 
 // Способ проверки покрытия данным билетом l-подмножеств множества S, за которое может выдаваться приз
 // S - множество номеров
@@ -21,4 +24,29 @@ set<int> check_coverage(const set<int> S, const set<int> &  l_subsets, const set
             // Если колличество элементов равно l
                 // ticket покрывает l_subset
                 // covered_l_subsets.append(l_subset)
+}
+
+vector<set<int>> generate_subsets(vector<int> superset, int k){
+    int n = superset.size();
+    int number_of_all_subsets = static_cast<int>(pow(2, n));
+    set<int> subset;
+    vector<set<int>> all_subsets;
+
+    for (size_t i = 0; i < number_of_all_subsets; ++i){
+        for (size_t j = 0; j < n; ++j){
+            if (i & (1 << j)){
+                subset.insert(superset[j]);            
+            }
+        }
+        if (subset.size() == k) all_subsets.push_back(subset);
+        subset.clear(); 
+    }
+
+    return all_subsets;
+}
+
+
+int main(){
+    vector<set<int>> all_subsets = generate_subsets({1,2,3,4,5}, 2);
+    return 0;
 }
