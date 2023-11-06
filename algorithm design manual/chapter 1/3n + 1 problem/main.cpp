b8f6cad10c6c04ade206ce9695f6eef00e6cfbb7
Arseny Bulaev <seni190999@mail.ru>
2023-11-06 12:54:38 +0300

    map solution

--- algorithm design manual/chapter 1/3n + 1 problem/main.cpp
+++ algorithm design manual/chapter 1/3n + 1 problem/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <map>
 
-int collatz_cycle_length(int n){
+int collatz_cycle_length(int n, const std::map<int,int> & previous_results){
     int cycle_length = 1;
     while(n != 1){
+        if (previous_results.find(n) != previous_results.end()){
+            cycle_length += previous_results.at(n) - 1;
+            break;
+        }
+        
         cycle_length += 1;
         if (n % 2 != 0){
             n = 3*n + 1;
@@ -14,9 +20,10 @@ int collatz_cycle_length(int n){
     return cycle_length;
 }
 
-
-int main(){
+void three_n_plus_one_problem(){
     int a, b;
+    std::map<int,int> previous_results;
+
     while((std::cin >> a >> b)){
         bool is_a_greater_than_b = false;
         if(a > b){
@@ -27,7 +34,8 @@ int main(){
         }
         int max_cycle_length = 0;
         for (int i = a; i <= b; ++i){
-            int cycle_length = collatz_cycle_length(i);
+            int cycle_length = collatz_cycle_length(i, previous_results);
+            previous_results[i] = cycle_length;
             if(cycle_length > max_cycle_length){
                 max_cycle_length = cycle_length;
             }
@@ -38,5 +46,9 @@ int main(){
         std::cout << max_cycle_length << std::endl;
         if(std::cin.eof()) break;
     }
+}
+
+int main(){
+    three_n_plus_one_problem();
     return 0;
 }
