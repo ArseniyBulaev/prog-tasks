ad042c09f241c64fb306727a8fd545ee48f222f5
Arseny Bulaev <seni190999@mail.ru>
2023-11-08 20:13:06 +0300

    mystery shit

--- algorithm design manual/chapter 1/the trip/main.cpp
+++ algorithm design manual/chapter 1/the trip/main.cpp
@@ -2,51 +2,76 @@
 #include <vector>
 #include <iomanip>
 #include <cmath>
+#include <fstream>
 
-double calculate_exchange_amount(const std::vector<float> & student_amounts){
+double average(const std::vector<double> & elements){
     double sum = 0;
-    double must_be_exchanged = 0;
-    for (size_t i = 0; i < student_amounts.size(); ++i){
-        sum += student_amounts[i];
+    for (size_t i = 0; i < elements.size(); ++i){
+        sum += elements[i];
     }
-    double avg = sum / student_amounts.size();
-    // std::cout << "avg without round " << avg << std::endl;
+    return sum / elements.size();
+}
+
+double rounding_to_two_digits(double value){
     // Округление до двух знаков после запятой
-    double fraction = avg - std::floor(avg);
-    if (fraction < 0.01){
-        avg = std::floor(avg);
-    }
-    else{
-        avg = std::round(avg * 100) / 100.0;
-    }
-    // std::cout << "avg after round: " << avg << std::endl;
+    return static_cast<int>(value * 100.00) / 100.00;
+}
+
+double calculate_exchange_amount_some_guy(const std::vector<double> & student_amounts){
+    if (student_amounts.empty()) return 0;
+
+    // Вычисление среднего
+    double avg = average(student_amounts);
+   
+    double pos = 0, neg = 0, diff = 0;
     for (double student_amount : student_amounts){
-        if(student_amount < avg){
-            must_be_exchanged += avg - student_amount;
-        }
+         double diff = rounding_to_two_digits(student_amount - avg);
+         if (diff >= 0) {
+				pos += diff;
+			} else {
+				neg += diff;
+			}
     }
-    return must_be_exchanged;
+
+    return (pos >= -neg) ? pos : -neg;
+}
+
+double calculate_exchange_amount_my(const std::vector<double> & student_amounts){
+    if (student_amounts.empty()) return 0;
+    // Вычисление среднего
+    double avg = average(student_amounts);
+    double to_exchange = 0;
+    for (double student_amount : student_amounts){
+         if(student_amount > avg){
+            to_exchange += rounding_to_two_digits(student_amount - avg);
+         }
+    }
+    return to_exchange;
 }
 
 void the_trip_problem(){
     int n;
     double student_amount;
-    while(std::cin >> n){
-        std::vector<float> student_amounts;
+    while(true){
+        std::cin >> n;
+        if(n == 0) break;
+        std::vector<double> student_amounts;
         for (size_t i = 0; i < n; ++i){
             std::cin >> student_amount;
             student_amounts.push_back(student_amount);
         }
-        double must_be_exchanged = calculate_exchange_amount(student_amounts);
-        if (must_be_exchanged > 0.0){
-            std::cout << "$" << std::fixed << std::setprecision(2) << must_be_exchanged << std::endl;
-            std::cout.unsetf(std::ios_base::floatfield);
-        }
-        if (std::cin.eof()) break;
+        double must_be_exchanged_my = calculate_exchange_amount_my(student_amounts);
+        double must_be_exchanged_some_guy = calculate_exchange_amount_some_guy(student_amounts);
+        std::cout << "$" << std::fixed << std::setprecision(2) << must_be_exchanged_some_guy << std::endl;
+        std::cout.unsetf(std::ios_base::floatfield);
+        std::cout << (must_be_exchanged_my == must_be_exchanged_some_guy) << std::endl;
     }
 }
 
 int main(){
+    std::ifstream in("input.txt");
+    std::streambuf *cinbuf = std::cin.rdbuf();
+    std::cin.rdbuf(in.rdbuf());
     the_trip_problem();
     return 0;
 }
