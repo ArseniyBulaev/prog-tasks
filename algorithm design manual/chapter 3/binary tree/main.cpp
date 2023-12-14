fef49801b216358d4b3c6f05e9a093969ba6c527
Arseny Bulaev <seni190999@mail.ru>
2023-12-14 19:49:00 +0300

    traverse_test

--- algorithm design manual/chapter 3/binary tree/main.cpp
+++ algorithm design manual/chapter 3/binary tree/main.cpp
@@ -3,14 +3,18 @@
 
 using std::cout, std::endl;
 
-int main(){
-    int array [] = {1, 6, 3, 1, 2, 6};
-    size_t array_size = sizeof(array) / sizeof(array[0]);
-    Tree root(array, array_size);
-    root.traverse([](Node * p) {
+void traverse_test(Tree & tree){
+    tree.traverse([](Node * p) {
         cout <<"I'm node with item " << p->item;
         cout <<". My addres " << p;
         cout << ". My parent addres is " << p->parent << endl;
     });
+}
+
+int main(){
+    int array [] = {1, 6, 3, 1, 2, 6};
+    size_t array_size = sizeof(array) / sizeof(array[0]);
+    Tree root(array, array_size);
+    
     return 0;
 }
