f4f7615ccd27f61484a3ccf257a3dbbdcaaa5507
Arseny Bulaev <seni190999@mail.ru>
2023-12-27 12:36:51 +0300

    random things

--- algorithm design manual/chapter 3/avl tree/main.cpp
+++ algorithm design manual/chapter 3/avl tree/main.cpp
@@ -8,27 +8,42 @@
 
 using std::cout, std::endl;
 
-std::vector<int> generate_random_vector(size_t size){
-    using namespace std;
-     // First create an instance of an engine.
-    random_device rnd_device;
-    // Specify the engine and distribution.
-    mt19937 mersenne_engine {rnd_device()};  // Generates random integers
-    uniform_int_distribution<int> dist {1, 100};
+std::vector<int> generate_random_vector(size_t size, int min_value, int max_value){
+   // First create an instance of an engine.
+   std::random_device rnd_device;
+   // Specify the engine and distribution.
+   std::mt19937 mersenne_engine {rnd_device()};  // Generates random integers
+   std::uniform_int_distribution<int> dist {min_value, max_value};
     
     auto gen = [&dist, &mersenne_engine](){
                    return dist(mersenne_engine);
                };
-
-    vector<int> vec(size);
+    std::vector<int> vec(size);
     generate(begin(vec), end(vec), gen);
     return vec;
 }
 
+int generate_random_value(int min_value, int max_value){
+    // First create an instance of an engine.
+   std::random_device rnd_device;
+   // Specify the engine and distribution.
+   std::mt19937 mersenne_engine {rnd_device()};  // Generates random integers
+   std::uniform_int_distribution<int> dist {min_value, max_value};
+    
+    auto gen = [&dist, &mersenne_engine](){
+                   return dist(mersenne_engine);
+               };
+    return gen();
+}
+
 int main(){
    size_t number_of_random_values = 5;
-   std::vector<int> random_numbers = generate_random_vector(number_of_random_values);
+   int min_value = 1;
+   int max_value = 100;
+   std::vector<int> random_numbers = generate_random_vector(number_of_random_values, min_value, max_value);
+   size_t random_indx = generate_random_value(0, random_numbers.size());
+   int random_item_from_numbers = random_numbers[random_indx]; 
    AVLTree tree(random_numbers);
-   AVLTree copy = tree;
+   tree.remove(random_item_from_numbers);
    return 0;
 }
