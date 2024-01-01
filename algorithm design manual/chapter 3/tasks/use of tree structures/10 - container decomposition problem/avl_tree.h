39bde71b39fadcb9fe5a32f83e7ab8bbbb836eba
Arseny Bulaev <seni190999@mail.ru>
2024-01-01 14:33:52 +0300

    remove unnecessary methods

---
+++ algorithm design manual/chapter 3/tasks/use of tree structures/10 - container decomposition problem/avl_tree.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstddef>
+#include <vector>
+
+struct AVLNode{
+    double weight;
+    size_t height = 0;
+    AVLNode * left = nullptr;
+    AVLNode * right = nullptr;
+    AVLNode(double weight):weight(weight), height(1) {}
+};
+
+class AVLTree{
+public:
+    ~AVLTree();
+private:
+    AVLNode * head = nullptr;
+    static size_t get_height(AVLNode * node);
+    static int get_balance_factor(AVLNode * node);
+    static void restore_height(AVLNode * node);
+    static AVLNode * left_rotation(AVLNode * node);
+    static AVLNode * right_rotation(AVLNode * node);
+    static AVLNode * balance(AVLNode * node);
+    static void delete_tree(AVLNode * node);
+    static AVLNode * create_from(AVLNode * node);
+    static AVLNode * find_min(AVLNode * node);
+    static AVLNode * remove_min(AVLNode * node);
+    AVLNode * insert(double weight, AVLNode * node);
+    AVLNode * remove(double weight, AVLNode * node);
+};
