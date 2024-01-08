e6653ee79c8adff0a63f8736269066f1d7587f32
Arseny Bulaev <seni190999@mail.ru>
2024-01-08 22:19:04 +0300

    move constructor to public section

--- algorithm design manual/chapter 3/tasks/use of tree structures/11 - smallest element of subsequence/smallest_friendly_array.h
+++ algorithm design manual/chapter 3/tasks/use of tree structures/11 - smallest element of subsequence/smallest_friendly_array.h
@@ -7,8 +7,8 @@ private:
     std::vector<int> elements;
     std::vector<std::vector<int>> smallest_elements_in_subseq;
     void expand_smallest_elements_in_subseq(int new_element);
-    SmallestFriendlyArray(const std::vector<int> & vec);
 public:
+    SmallestFriendlyArray(const std::vector<int> & vec);
     void insert(int element);
     int get_smallest_between(size_t i, size_t j);
 };
