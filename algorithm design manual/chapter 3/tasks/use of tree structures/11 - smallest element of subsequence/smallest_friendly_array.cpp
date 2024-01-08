2676f4f92d496afd65c533339babd5692fff7e02
Arseny Bulaev <seni190999@mail.ru>
2024-01-08 22:55:05 +0300

    +- works

--- algorithm design manual/chapter 3/tasks/use of tree structures/11 - smallest element of subsequence/smallest_friendly_array.cpp
+++ algorithm design manual/chapter 3/tasks/use of tree structures/11 - smallest element of subsequence/smallest_friendly_array.cpp
@@ -5,17 +5,25 @@ void SmallestFriendlyArray::expand_smallest_elements_in_subseq(int new_element){
     size_t indx_of_new_element = elements.size();
     for(std::vector<int> & subseq_for_some_element : smallest_elements_in_subseq){
         // Индекс наименьшего элемента в подпоследовательности  x_i ... x_n 
-        // равен либо индексу текущему минимума, либо индексу нового элемента 
-        int back = subseq_for_some_element.back();
-        size_t indx_of_smallest = new_element < back ? indx_of_new_element : back;
+        // равен либо индексу текущему минимума, либо индексу нового элемента
+        size_t indx_of_smallest = indx_of_new_element;
+        if(!subseq_for_some_element.empty()){
+            int back = subseq_for_some_element.back();
+            if(elements[back] < new_element) indx_of_smallest = back;
+        }
         subseq_for_some_element.push_back(indx_of_smallest);
     } 
      // Добавляем новый элемент в список элементов
     elements.push_back(new_element);
+    smallest_elements_in_subseq.push_back({indx_of_new_element});
 }
 
 
 SmallestFriendlyArray::SmallestFriendlyArray(const std::vector<int> & vec){
+    // Инициализация
+    elements = std::vector<int>();
+    smallest_elements_in_subseq = std::vector<std::vector<int>>();
+
     for(int element : vec){
         insert(element);
     }
@@ -27,5 +35,6 @@ void SmallestFriendlyArray::insert(int element){
 }
 
 int SmallestFriendlyArray::get_smallest_between(size_t i, size_t j){
-    return smallest_elements_in_subseq[i][j - i - 1];
+    size_t indx_of_smallest = smallest_elements_in_subseq[i][j - i];
+    return elements[indx_of_smallest];
 }
