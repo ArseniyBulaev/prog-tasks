685c9a06b4187705ac43baf5169fceabbe061f65
Arseny Bulaev <seni190999@mail.ru>
2024-02-07 18:21:46 +0300

    add test

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/13 - some array representation/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/13 - some array representation/main.cpp
@@ -8,22 +8,12 @@
 
 using std::cout, std::endl;
 
-std::vector<int> generate_random_vector(size_t size, int min_value, int max_value){
-   // First create an instance of an engine.
-   std::random_device rnd_device;
-   // Specify the engine and distribution.
-   std::mt19937 mersenne_engine {rnd_device()};  // Generates random integers
-   std::uniform_int_distribution<int> dist {min_value, max_value};
-    
-    auto gen = [&dist, &mersenne_engine](){
-                   return dist(mersenne_engine);
-               };
-    std::vector<int> vec(size);
-    generate(begin(vec), end(vec), gen);
-    return vec;
-}
 
-int generate_random_value(int min_value, int max_value){
+void add_test(){
+    SomeTree array_tree;
+    size_t test_size = 100;
+    int min_value = 0;
+    int max_value = test_size - 1;
     // First create an instance of an engine.
    std::random_device rnd_device;
    // Specify the engine and distribution.
@@ -33,16 +23,30 @@ int generate_random_value(int min_value, int max_value){
     auto gen = [&dist, &mersenne_engine](){
                    return dist(mersenne_engine);
                };
-    return gen();
+    std::vector<int> random_test(test_size);
+    generate(begin(random_test), end(random_test), gen);
+
+    for(int value : random_test){
+        array_tree.insert(value);
+    }
+
+    for(size_t i = 0; i <= test_size; ++i){
+        size_t indx = gen();
+        int value = gen();
+        random_test[indx] += value;
+        array_tree.add(indx, value);
+        Node * node = array_tree.find(indx);
+        if(node != nullptr){
+            if (node->value != random_test[indx]){
+                cout << "node value: " << node->value;
+                cout << " vector value: " << random_test[indx] << endl;
+            }
+        } 
+    }
+
 }
 
-
 int main(){
-   SomeTree array_tree;
-   std::vector<int> test = {10, 12, 3, 5, 1};
-   for(int value : test){
-        array_tree.insert(value);
-   }
-   array_tree.add(0, 4);
+   add_test();
    return 0;
 }
