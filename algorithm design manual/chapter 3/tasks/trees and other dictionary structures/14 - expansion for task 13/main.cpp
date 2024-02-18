2108b132e908a866cc73194c7c7687e7377373ba
Arseny Bulaev <seni190999@mail.ru>
2024-02-18 19:16:39 +0300

    copy code from task 13 and expand insert logic

---
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/14 - expansion for task 13/main.cpp
@@ -0,0 +1,67 @@
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <vector>
+
+#include "some_tree.h"
+
+using std::cout, std::endl;
+
+
+void add_test(){
+    SomeTree array_tree;
+    size_t test_size = 100;
+    int min_value = 0;
+    int max_value = test_size - 1;
+    // First create an instance of an engine.
+   std::random_device rnd_device;
+   // Specify the engine and distribution.
+   std::mt19937 mersenne_engine {rnd_device()};  // Generates random integers
+   std::uniform_int_distribution<int> dist {min_value, max_value};
+    
+    auto gen = [&dist, &mersenne_engine](){
+                   return dist(mersenne_engine);
+               };
+    std::vector<int> random_test(test_size);
+    generate(begin(random_test), end(random_test), gen);
+
+    for(int value : random_test){
+        array_tree.insert(value);
+    }
+
+    for(size_t i = 0; i <= test_size; ++i){
+        size_t indx = gen();
+        int value = gen();
+        random_test[indx] += value;
+        array_tree.add(indx, value);
+        Node * node = array_tree.find(indx);
+        if(node != nullptr){
+            if (node->value != random_test[indx]){
+                cout << "node value: " << node->value;
+                cout << " vector value: " << random_test[indx] << endl;
+            }
+        } 
+    }
+
+}
+
+int main(){
+   //add_test();
+   SomeTree array_tree;
+   std::vector<int> test = {7, 5, 3, 1, 2, 10, 3 , 3};
+   for(int value : test){
+        array_tree.insert(value);
+   }
+
+   for(size_t i = 0; i <= test.size(); ++i){
+        cout << i << " " << array_tree.partial_sum(i) << endl;
+   }
+
+   array_tree.add(4, 2);
+
+   for(size_t i = 0; i <= test.size(); ++i){
+        cout << i << " " << array_tree.partial_sum(i) << endl;
+   }
+   return 0;
+}
---
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/14 - expansion for task 13/some_tree.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstddef>
+#include <cmath>
+
+using std::size_t;
+
+struct Node{
+    int key;
+    int value;
+    int sum_in_left_subtree = 0;
+    int sum_in_right_subtree = 0;
+    size_t height = 0;
+    Node * left = nullptr;
+    Node * right = nullptr;
+    Node(int key, int value): key(key), value(value), height(1) {}
+};
+
+class SomeTree{
+public:
+    size_t size() { return tree_size;};
+    void insert(int value, int key = -1);
+    Node * find(size_t i);
+    void add(size_t i, int y);
+    int partial_sum(size_t i);
+    ~SomeTree();
+private:
+    Node * head = nullptr;
+    size_t tree_size = 0;
+    static size_t get_height(Node * node);
+    static int get_balance_factor(Node * node);
+    static void restore_height(Node * node);
+    static Node * left_rotation(Node * node);
+    static Node * right_rotation(Node * node);
+    static Node * balance(Node * node);
+    static void delete_tree(Node * node);
+    Node * insert(int key, int value, Node * node);
+    void add(size_t i, int y, Node * node);
+    Node * find(size_t i, Node * node);
+    std::pair<Node *, Node *> find(size_t i, Node * node, Node * parent);
+    int partial_sum(size_t i, Node * node);
+};
