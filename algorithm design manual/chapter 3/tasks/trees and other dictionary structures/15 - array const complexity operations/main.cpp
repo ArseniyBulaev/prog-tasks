d3b6b520f20cc976d18c304cb2715d357c90c0fc
Arseny Bulaev <seni190999@mail.ru>
2024-02-23 14:52:17 +0300

    quick search test

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/main.cpp
@@ -1,9 +1,14 @@
+#include <iostream>
+
 #include "const_op_array.h"
 
+using std::cout, std::endl;
 
 int main(){
     size_t n = 10;
     size_t m = 5;
     ConstOpArray coparray(n, m);
+    cout << coparray.search(21) << endl;
+    cout << coparray.search(6) << endl;
     return 0;
 }
