d55db8defff671eeb437593cda7634b3cd9bc076
Arseny Bulaev <seni190999@mail.ru>
2024-02-23 14:31:21 +0300

    initialize 'initialized locations' with false by default + comments

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/const_op_array.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/const_op_array.cpp
@@ -3,10 +3,10 @@
 ConstOpArray::ConstOpArray(size_t n, size_t m){
     locations = new size_t[n];
     elements = new int[m];
-    initialized_locations = new bool[n];
+    initialized_locations = new bool[n]{false}; // Сначала ничего не инициализировано
     available_locations = std::stack<size_t>();
     for (size_t i = 0; i < m; ++i){
-        available_locations.push(i);
+        available_locations.push(i); // При создании доступны все ячейки
     }
 }
 
