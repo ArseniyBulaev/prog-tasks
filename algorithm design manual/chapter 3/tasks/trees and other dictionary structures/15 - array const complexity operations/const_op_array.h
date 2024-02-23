54726cc95e393617f0e8ae610512da245fb837bc
Arseny Bulaev <seni190999@mail.ru>
2024-02-23 14:20:26 +0300

    constructor

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/const_op_array.h
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/const_op_array.h
@@ -3,6 +3,15 @@
 
 class ConstOpArray{
 public:
+    ConstOpArray(size_t n, size_t m){
+        locations = new size_t[n];
+        elements = new int[m];
+        initialized_locations = new bool[n];
+        available_locations = std::stack<size_t>();
+        for (size_t i = 0; i < m; ++i){
+            available_locations.push(i);
+        }
+    }
 private:
     size_t n; // Правая граница диапазона
     size_t m; // Число элементов, которые можно хранить
