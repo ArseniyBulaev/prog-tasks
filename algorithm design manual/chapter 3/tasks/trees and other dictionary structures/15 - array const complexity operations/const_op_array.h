43407e1719452cfdf071c357544474bd220e097e
Arseny Bulaev <seni190999@mail.ru>
2024-02-23 14:15:58 +0300

    change types for n, m

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/const_op_array.h
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/const_op_array.h
@@ -2,9 +2,10 @@
 #include <cstddef>
 
 class ConstOpArray{
+public:
 private:
-    int n; // Правая граница диапазона
-    int m; // Число элементов, которые можно хранить
+    size_t n; // Правая граница диапазона
+    size_t m; // Число элементов, которые можно хранить
     size_t * locations; // Позиции элементов в массиве B (Массив A)
     int * elements; // Элементы (Массив B)
     bool * initialized_locations; // Инициальзированные позиции
