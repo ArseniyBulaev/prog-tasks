eddbcfcfe6ab0625b8764814211570e780306593
Arseny Bulaev <seni190999@mail.ru>
2024-02-24 22:27:04 +0300

    seems to work

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/15 - array const complexity operations/main.cpp
@@ -8,14 +8,5 @@ int main(){
     size_t n = 10;
     size_t m = 5;
     ConstOpArray coparray(n, m);
-    coparray.insert(2);
-    coparray.insert(2);
-    coparray.insert(1);
-    coparray.insert(10);
-    coparray.insert(9);
-
-    cout << coparray.search(9) << endl;
-    cout << coparray.search(2) << endl;
-    cout << coparray.search(1) << endl;
     return 0;
 }
