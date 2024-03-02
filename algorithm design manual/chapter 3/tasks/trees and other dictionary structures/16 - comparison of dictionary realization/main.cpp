99afc4b4bb0cc685116c1837a529cdc6e63eb93f
Arseny Bulaev <seni190999@mail.ru>
2024-03-02 16:47:29 +0300

    read test text

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/16 - comparison of dictionary realization/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/16 - comparison of dictionary realization/main.cpp
@@ -1,3 +1,16 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+std::stringstream get_text(const std::string & file_name){
+    std::ifstream file_with_text(file_name);
+    std::stringstream buffer;
+    buffer << file_with_text.rdbuf();
+    return buffer;
+}
+
+
 int main(){
     return 0;
 }
