6066f3d96d674bb2ee00806fa22169d0022f2586
Arseny Bulaev <seni190999@mail.ru>
2024-03-03 18:11:57 +0300

    realisation of binary tree

---
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/16 - comparison of dictionary realization/binary_tree.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstddef>
+
+struct Node
+{
+    int item;
+    Node * parent = nullptr;
+    Node * left = nullptr;
+    Node * right = nullptr;
+};
+
+
+class Tree{
+private:
+    Node * head = nullptr;
+    void traverse(void (*operation) (Node * node_p), Node * node_p);
+    Node * copy(const Node & copy_from, Node * parent);
+    Node * search(Node * current, int target) const;
+    void remove_if_leaf(Node *);
+    void remove_if_one_child(Node *);
+    void remove_if_two_children(Node *);
+
+public:
+    Tree() = default;
+    Tree(int * array, size_t array_size);
+    Tree(const Tree & copy_from);
+    void insert(Node ** came_from, int item, Node * parent);
+    Node * search(int target) const;
+    Node * minimum() const;
+    Node * maximum() const;
+    void traverse(void (*operation) (Node * node_p));
+    void remove(int item);
+    ~Tree();
+};
+
