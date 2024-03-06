bf072c48b0a38f0c6c6d0038c8d88b4c50b698b6
Arseny Bulaev <seni190999@mail.ru>
2024-03-06 18:53:13 +0300

    generate reverse alphabet too

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/17 - deciphering the Caesar cipher/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/17 - deciphering the Caesar cipher/main.cpp
@@ -5,15 +5,19 @@
 #include <string>
 
 
-std::unordered_map<char, int> generate_english_alphabet(){
-    std::unordered_map<char, int> alphabet;
+void generate_english_alphabet(std::unordered_map<char, int> & alphabet,
+std::unordered_map<int, char> & reverse_alphabet){
     for(char letter = 'a'; letter <= 'z'; ++letter){
-        alphabet[letter] = letter - 'a';
+        int letter_number = letter - 'a';
+        alphabet[letter] = letter_number;
+        reverse_alphabet[letter_number] = letter;
     }
-    return alphabet;
 }
 
+
 int main(){
-    std::unordered_map<char, int> alphabet = generate_english_alphabet();
+    std::unordered_map<char, int> alphabet;
+    std::unordered_map<int, char> reverse_alphabet;
+    generate_english_alphabet(alphabet, reverse_alphabet);
     return 0;
 }
