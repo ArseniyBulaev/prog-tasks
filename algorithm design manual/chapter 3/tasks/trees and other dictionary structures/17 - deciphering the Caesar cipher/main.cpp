fd103d6015b0964fd3e4650a40c6d6a256ed7ccd
Arseny Bulaev <seni190999@mail.ru>
2024-03-06 18:43:49 +0300

    english alphabet generation

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/17 - deciphering the Caesar cipher/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/17 - deciphering the Caesar cipher/main.cpp
@@ -5,6 +5,15 @@
 #include <string>
 
 
+std::unordered_map<char, int> generate_english_alphabet(){
+    std::unordered_map<char, int> alphabet;
+    for(char letter = 'a'; letter <= 'z'; ++letter){
+        alphabet[letter] = letter - 'a';
+    }
+    return alphabet;
+}
+
 int main(){
+    std::unordered_map<char, int> alphabet = generate_english_alphabet();
     return 0;
 }
