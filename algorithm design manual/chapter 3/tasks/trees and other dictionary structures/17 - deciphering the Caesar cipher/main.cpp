2cdd344f3150467c97d2d53cca5bb41ee6660032
Arseny Bulaev <seni190999@mail.ru>
2024-03-07 12:03:52 +0300

    english letter frequency

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/17 - deciphering the Caesar cipher/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/17 - deciphering the Caesar cipher/main.cpp
@@ -105,6 +105,35 @@ std::unordered_map<char, double> calculate_letter_frequency(const std::string & 
 }
 
 int main(){
+    std::unordered_map<char, double> english_letter_frequency = {
+        {'e', 12.7},
+        {'t', 9.06},
+        {'a', 8.17},
+        {'o', 7.51},
+        {'i', 6.97},
+        {'n', 6.75},
+        {'s', 6.33},
+        {'h', 6.09},
+        {'r', 5.99},
+        {'d', 4.25},
+        {'l', 4.03},
+        {'c', 2.78},
+        {'u', 2.76},
+        {'m', 2.41},
+        {'w', 2.36},
+        {'f', 2.23},
+        {'g', 2.02},
+        {'y', 1.97},
+        {'p', 1.93},
+        {'b', 1.49},
+        {'v', 0.98},
+        {'k', 0.77},
+        {'x', 0.15},
+        {'j', 0.15},
+        {'q', 0.1},
+        {'z', 0.7},
+    };
+    
     std::unordered_map<char, int> alphabet;
     std::unordered_map<int, char> reverse_alphabet;
     const std::string text_path = "text\\input.txt";
