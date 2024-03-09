1f09ea8312804eb1b19b976c51a7b21cd8d99b85
Arseny Bulaev <seni190999@mail.ru>
2024-03-09 12:24:36 +0300

    fix

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/20 - mid of list/source/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/20 - mid of list/source/main.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <iostream>
+#include <cmath>
 
 struct Node{
     Node * next = nullptr;
@@ -8,8 +9,8 @@ struct Node{
 
 class List{
 private:
-    Node * _head;
-    size_t _size;
+    Node * _head = nullptr;
+    size_t _size = 0;
 public:
     size_t size(){
         return _size;
@@ -19,11 +20,28 @@ public:
         _head = new_node;
         ++_size;
     }
+    int find_mid(){
+        // Если элементов чётное число, то средним элементом считаем size() / 2
+        size_t mid_indx = static_cast<size_t>(ceil(size() / 2.0));
+        size_t current_indx = 1;
+        Node * current = _head;
+        while(current_indx < mid_indx){
+            current = current->next;
+            ++current_indx;
+        }
+        if(current != nullptr){
+            return current->data;
+        } 
+        else{
+            std::cout << "the list is empty ";
+            return -1;
+        }
+    }
     ~List(){
         Node * current = _head;
         while(current != nullptr){
             Node * buf = current->next;
-            std::cout << current->data << std::endl;
+            std::cout << "delete " << current->data << std::endl;
             delete current;
             current = buf;
         }
@@ -33,8 +51,9 @@ public:
 
 int main(){
     List list;
-    for(size_t i = 1; i <= 10; ++i){
+    for(size_t i = 1; i <= 3; ++i){
         list.insert(i);
     }
+    std::cout << "mid of the list: " << list.find_mid() << std::endl;
     return 0;
 }
