f9959f9868f37637e63bd7ffb8f1a859def4f88e
Arseny Bulaev <seni190999@mail.ru>
2024-03-16 19:17:05 +0300

    flip a string

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/26 - reverse word order/source/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/26 - reverse word order/source/main.cpp
@@ -24,10 +24,18 @@ std::vector<char> transform_string_to_vector(const std::string & str){
     return std::vector<char>(str.begin(), str.end());
 }
 
+
+void flip(std::vector<char> & str){
+    size_t size = str.size();
+    for(size_t i = 0; i < size / 2; ++i){
+        std::swap(str[i], str[size - i - 1]);
+    }
+}
+
 int main(){
     const std::string input_string_path = "..\\input\\sample.txt";
     std::string input = read_text(input_string_path);
     std::vector<char> input_v = transform_string_to_vector(input);
-    
+    flip(input_v);
     return 0;
 }
