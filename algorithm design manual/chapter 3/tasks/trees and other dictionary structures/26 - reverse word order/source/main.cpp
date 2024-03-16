c61702f89c515e4b7f111145c73cfbfa83878367
Arseny Bulaev <seni190999@mail.ru>
2024-03-16 19:23:57 +0300

    start pos and size params

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/26 - reverse word order/source/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/26 - reverse word order/source/main.cpp
@@ -25,17 +25,21 @@ std::vector<char> transform_string_to_vector(const std::string & str){
 }
 
 
-void flip(std::vector<char> & str){
-    size_t size = str.size();
-    for(size_t i = 0; i < size / 2; ++i){
-        std::swap(str[i], str[size - i - 1]);
+void flip(std::vector<char> & str, size_t size, size_t start = 0){
+    for(size_t i = start; i < size / 2; ++i){
+        std::swap(str[i + start], str[size - i - 1]);
     }
 }
+
+void reverse_word_order(std::vector<char> & str){
+
+}
+
 
 int main(){
     const std::string input_string_path = "..\\input\\sample.txt";
     std::string input = read_text(input_string_path);
     std::vector<char> input_v = transform_string_to_vector(input);
-    flip(input_v);
+    flip(input_v, input_v.size());
     return 0;
 }
