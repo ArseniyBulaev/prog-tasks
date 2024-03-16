7e8cd2d912698d9a8bad6f9d3b2be327b5ebb4e7
Arseny Bulaev <seni190999@mail.ru>
2024-03-16 21:17:08 +0300

    make a loop in list

--- algorithm design manual/chapter 3/tasks/trees and other dictionary structures/27 - is there a loop in list/source/main.cpp
+++ algorithm design manual/chapter 3/tasks/trees and other dictionary structures/27 - is there a loop in list/source/main.cpp
@@ -1,10 +1,17 @@
 #include <unordered_set>
 #include "list.h"
 
+
+
+
 int main(){
     char * input [] = {"a", "b", "c"};
     size_t size = sizeof(input) / sizeof(input[0]);
     List lst(input, size);
+    lst.print();
+    Node * a = lst.search("a");
+    Node * c = lst.search("c");
+    c->next = a;
     lst.print();
     return 0;
 }
