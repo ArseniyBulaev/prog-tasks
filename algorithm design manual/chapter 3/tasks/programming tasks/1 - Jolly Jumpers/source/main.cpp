3fea15c2d028dc0cb173f36770e9fa121a179ad8
Arseny Bulaev <seni190999@mail.ru>
2024-03-17 22:54:06 +0300

    first version

--- algorithm design manual/chapter 3/tasks/programming tasks/1 - Jolly Jumpers/source/main.cpp
+++ algorithm design manual/chapter 3/tasks/programming tasks/1 - Jolly Jumpers/source/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <fstream>
+#include <vector>
 #include <sstream>
 #include <iostream>
 #include <iterator>
@@ -24,11 +25,30 @@ std::string read_text(const std::string & file_path){
 }
 
 
-
+bool is_it_jolly(const std::vector<int> & sample){
+    const int n = sample[0];
+    // The definition implies that any sequence of a single integer is a jolly jumper
+    if(n == 1) return true;
+    std::vector<bool> jolly_condition(n - 1, false);
+    for(size_t i = 1; i < n; ++i){
+        size_t possible_part_of_jolly_condition = abs(sample[i] - sample[i + 1]);
+        if(possible_part_of_jolly_condition <= n){
+            jolly_condition[possible_part_of_jolly_condition - 1] = true;
+        }
+    }
+    return std::all_of(jolly_condition.begin(), jolly_condition.end(), [](bool v) { return v; });
+}
 
 
 int main(){
-    const std::string text_file_path = "..\\input\\input.txt";
+    const std::string input_file_path = "..\\input\\input.txt";
+    const std::string input = read_text(input_file_path);
+    std::stringstream input_stream(input);
+    std::string line;
+    while(getline(input_stream, line)){
+        std::stringstream line_stream(line);
+        std::cout << (is_it_jolly(std::vector<int>(std::istream_iterator<int>(line_stream),{})) ? "Jolly" : "Not jolly") << std::endl;
+    }
     
     return 0;
 }
