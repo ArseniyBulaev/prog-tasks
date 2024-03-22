6346968db740bb3051252939863fc8354d99a108
Arseny Bulaev <seni190999@mail.ru>
2024-03-22 22:40:38 +0300

    accepted

--- algorithm design manual/chapter 3/tasks/programming tasks/4 - Crypt Kicker 2/source/main.cpp
+++ algorithm design manual/chapter 3/tasks/programming tasks/4 - Crypt Kicker 2/source/main.cpp
@@ -7,8 +7,7 @@
 
 std::vector<std::string> read_case(){
     std::vector<std::string> lines;
-    std::string line, blank;
-    std::getline(std::cin, blank); 
+    std::string line, blank; 
     while(std::getline(std::cin, line) && !line.empty()){
         lines.push_back(line);
     }
@@ -25,16 +24,28 @@ bool are_there_gaps_in_the_same_places(const std::string & reference, const std:
 std::vector<std::string> process_case(const std::vector<std::string> & input_case){
     const std::string pangram = "the quick brown fox jumps over the lazy dog";
     std::unordered_map<char, char> permutation;
+    std::unordered_map<char, char> reverse_permutation;
     std::vector<std::string> output;
     bool pangram_found = false;
     for(const std::string & line: input_case){
+        if(pangram_found) break;
         if((line.size() == pangram.size()) && are_there_gaps_in_the_same_places(pangram, line)){
+            // Possible pangram found,
+            // but we need to check that no two letters are replaced by the same letter
+            pangram_found = true;
             // Make permutation dictionary
             for(size_t i = 0; i < line.size(); ++i){
-                permutation[line[i]] = pangram[i];
+                // if no two letters are replaced by the same letter
+                if((permutation.find(line[i]) == permutation.end())||(reverse_permutation[pangram[i]] == line[i])){
+                    permutation[line[i]] = pangram[i];
+                    reverse_permutation[pangram[i]] = line[i];
+                }
+                else{
+                    permutation.clear();
+                    pangram_found = false;
+                    break;
+                }
             }
-            pangram_found = true;
-            break;
         }
     }
 
@@ -63,12 +74,14 @@ int main(){
     std::cin >> number_of_cases;
     std::string blank;
     std::getline(std::cin, blank); // read rest of first line
+    std::getline(std::cin, blank); // read first blank line
     for (size_t i = 0; i < number_of_cases; ++i){
        std::vector<std::string> input_case = read_case();
        std::vector<std::string> output_case = process_case(input_case);
+       for(size_t j = 0; j < output_case.size(); ++j){
+           std::cout << output_case[j] << std::endl;
+       }
+       if(i != (number_of_cases - 1)) std::cout << '\n';
     }
-    
-    
-    
     return 0;
 }
