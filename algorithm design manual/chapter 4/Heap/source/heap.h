f4726c2d6b5c0f26b5d7116f9c8168939b567bc8
Arseny Bulaev <seni190999@mail.ru>
2024-03-24 14:24:22 +0300

    heap creation

--- algorithm design manual/chapter 4/Heap/source/heap.h
+++ algorithm design manual/chapter 4/Heap/source/heap.h
@@ -7,11 +7,31 @@ template <typename T>
 class Heap{
 public:
     Heap(): elements(std::vector<T>()) {};
-    void insert(T item);
+    Heap(const std::vector<T> & elements){
+        for(T element: elements) insert(element);
+    }
+    void insert(T item){
+        elements.push_back(item);
+        bubble_up(n);
+        ++n;
+    }
 private:
     size_t n = 0;
     std::vector<T> elements;
-    int get_parent_indx(size_t n);
-    size_t young_child(size_t n);
-    void bubble_up(size_t item_indx);
+    int get_parent_indx(size_t n){
+        // First element doesn't have a parent
+        if(n == 0) return -1;
+        else return (static_cast<int>(n / 2));
+    }
+    size_t young_child(size_t n){
+        return 2 * n;
+    }
+    void bubble_up(size_t item_indx){
+        int parent_indx = get_parent_indx(item_indx);
+        if(parent_indx == -1) return; // item is root node
+        else if (elements[parent_indx] > elements[item_indx]){
+            std::swap(elements[parent_indx], elements[item_indx]);
+            bubble_up(parent_indx);
+        }
+    }
 };
--- algorithm design manual/chapter 4/Heap/source/main.cpp
+++ algorithm design manual/chapter 4/Heap/source/main.cpp
@@ -3,6 +3,7 @@
 
 
 int main(){
-	Heap<int> heap;
+	std::vector<int> dates = {1783, 1804, 1918, 2001, 1865, 1941, 1776, 1945, 1492, 1963};
+	Heap<int> heap(dates);
 	return 0;
 }
