3251a9a30503dbf44350620ae2f864097196681c
Arseny Bulaev <seni190999@mail.ru>
2024-03-31 18:27:54 +0300

    move random staff to globals

--- algorithm design manual/chapter 4/bolts and nuts/source/main.cpp
+++ algorithm design manual/chapter 4/bolts and nuts/source/main.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
+#include <unordered_map>
+
+// Task size
+const size_t task_size = 10;
+// Random staff
+auto rd = std::random_device {}; 
+const auto rng = std::default_random_engine { rd() };
+const std::uniform_int_distribution<std::mt19937::result_type> dist_task_size(0, task_size - 1);
 
 int partition(std::vector<int> & s, int l, int h){
 	int p; // Ведущий элемент
@@ -19,17 +27,18 @@ int partition(std::vector<int> & s, int l, int h){
 }
 
 
+void find_pairs(const std::vector<int> & nuts, const std::vector<int> & bolts){
+
+}
+
 
 int main(){
 	// Create
-	size_t task_size = 10;
 	std::vector<int> bolts(task_size);
 	std::iota(bolts.begin(), bolts.end(), 1);
 	std::vector<int> nuts(task_size);
 	std::iota(nuts.begin(), nuts.end(), 1);
 	// Shuffle
-	auto rd = std::random_device {}; 
-	auto rng = std::default_random_engine { rd() };
 	std::shuffle(std::begin(bolts), std::end(bolts), rng);
 	std::shuffle(std::begin(nuts), std::end(nuts), rng);
 
