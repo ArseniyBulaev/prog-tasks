a08aaa0c7abfbd3acacb5793263e0c5e547f455c
Arseny Bulaev <seni190999@mail.ru>
2024-03-31 18:18:01 +0300

    shuffle bolts and nuts

--- algorithm design manual/chapter 4/bolts and nuts/source/main.cpp
+++ algorithm design manual/chapter 4/bolts and nuts/source/main.cpp
@@ -21,11 +21,17 @@ int partition(std::vector<int> & s, int l, int h){
 
 
 int main(){
+	// Create
 	size_t task_size = 10;
 	std::vector<int> bolts(task_size);
 	std::iota(bolts.begin(), bolts.end(), 1);
 	std::vector<int> nuts(task_size);
 	std::iota(nuts.begin(), nuts.end(), 1);
+	// Shuffle
+	auto rd = std::random_device {}; 
+	auto rng = std::default_random_engine { rd() };
+	std::shuffle(std::begin(bolts), std::end(bolts), rng);
+	std::shuffle(std::begin(nuts), std::end(nuts), rng);
 
 	return 0;
 } 
