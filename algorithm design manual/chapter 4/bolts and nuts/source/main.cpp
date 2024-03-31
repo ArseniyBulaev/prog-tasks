774edffeb7196ae6a8cb1b6c922eaee91ab0ab2a
Arseny Bulaev <seni190999@mail.ru>
2024-04-01 00:11:30 +0300

    p should equals to h

--- algorithm design manual/chapter 4/bolts and nuts/source/main.cpp
+++ algorithm design manual/chapter 4/bolts and nuts/source/main.cpp
@@ -6,15 +6,14 @@
 #include <cassert>
 
 // Task size
-const size_t task_size = 3;
+const size_t task_size = 10;
 // Random staff
 auto rd = std::random_device {}; 
 auto rng = std::default_random_engine { rd() };
 
-int partition(std::vector<int> & s, int l, int h){
-	int p; // Ведущий элемент
+int partition(std::vector<int> & s, int l, int h, int p){
 	int firsthigh; // Позиция для элементра разделителя
-	p = h;
+	assert((void("p should equals to h"), p == h));
 	firsthigh = l;
 	for(int i = l; i < h; i++){
 		if(s[i] < s[p]){
@@ -37,6 +36,16 @@ int main(){
 	std::shuffle(std::begin(bolts), std::end(bolts), rng);
 	std::shuffle(std::begin(nuts), std::end(nuts), rng);
 	
+	for(int x: nuts){
+		std::cout << x << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "pivot " << nuts[nuts.size() - 1] << std::endl;
+	std::cout << "partition returned " << partition(nuts, 0, nuts.size() - 1, nuts.size() - 1) << std::endl;
+	for(int x: nuts){
+		std::cout << x << " ";
+	}
+	std::cout << std::endl;
 
 	return 0;
 } 
