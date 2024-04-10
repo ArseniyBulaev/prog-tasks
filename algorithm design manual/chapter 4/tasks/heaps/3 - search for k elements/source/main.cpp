602010dcfb80413c81e4ea88012a92e1e2bfca36
Arseny Bulaev <seni190999@mail.ru>
2024-04-10 19:31:16 +0300

    get k smallest elements

--- algorithm design manual/chapter 4/tasks/heaps/3 - search for k elements/source/main.cpp
+++ algorithm design manual/chapter 4/tasks/heaps/3 - search for k elements/source/main.cpp
@@ -6,7 +6,19 @@
 
 
 int main(){
+	size_t k = 5;
 	std::vector<int> dates = {1783, 1804, 1918, 2001, 1865, 1941, 1776, 1945, 1492, 1963};
+	std::vector<int> sorted_dates = heap_sort(dates);
+	for(int date: sorted_dates){
+		std::cout << date << std::endl;
+	}
+	Heap heap(dates);
+	std::cout << k << " smallest elements:: ";
+	for (size_t i = 1; i <= k; i++){
+		std::cout << heap.extract_min();
+		if(i != k) std::cout << " ";
+	}
+	std::cout << std::endl;
 	
 	return 0;
 }
