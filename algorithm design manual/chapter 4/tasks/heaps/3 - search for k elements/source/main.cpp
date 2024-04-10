d49c332684967d9fba82212297142484cda1417c
Arseny Bulaev <seni190999@mail.ru>
2024-04-10 19:03:03 +0300

    fix possible access outside the array

---
+++ algorithm design manual/chapter 4/tasks/heaps/3 - search for k elements/source/main.cpp
@@ -0,0 +1,12 @@
+#include <cstddef>
+#include <iostream>
+
+#include "heap.h"
+
+
+
+int main(){
+	std::vector<int> dates = {1783, 1804, 1918, 2001, 1865, 1941, 1776, 1945, 1492, 1963};
+	
+	return 0;
+}
